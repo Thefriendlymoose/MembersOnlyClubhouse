46270797b11b845bbd683006e52f92b7ee3aede8
Thefriendlymoose <alamiridavid@gmail.com>
2022-01-25 22:26:23 +0100

    prepare for deploy

--- app.js
+++ app.js
@@ -47,14 +47,21 @@ app.use(function(req, res, next) {
 });
 
 
-
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
-
 
 app.use('/', clubhouseRouter);
 
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 
-
-app.listen(3000, () => console.log("Listening on port 3000!"));
+  module.exports = app;
